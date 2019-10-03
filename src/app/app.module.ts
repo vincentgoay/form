3e984811387ee7efcfd748f02c2a0e10a6a5c530
Vincent Goay <vinc3nt86@hotmail.com>
2019-10-03 10:44:32 +0800

    Created Registration Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
